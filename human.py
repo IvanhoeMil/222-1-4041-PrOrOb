ebaa6197ffdf9be2fbf12ea3967dd6733e0a97f3
Ivanhoemil <ivanodesantiago@gmail.com>
2021-09-30 17:00:45 -0500

    La primera clase de Python

---
+++ human.py
@@ -0,0 +1,16 @@
+class Human:
+
+    born_year = 1999
+
+    def say_hello(self):
+        print("Hello")
+
+    def say_hello_user(self, user_name):
+        print("Hello "+ user_name)
+
+human_one = Human()
+print(human_one)
+print(human_one.born_year)
+
+human_one.say_hello()#aqui estamos invocando
+human_one.say_hello_user("Ivan")#aqui estamos invocando
