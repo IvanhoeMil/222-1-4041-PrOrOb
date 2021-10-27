567c3204f5aa124a2daff7f7d24613ebfea54efb
Ivanhoemil <ivanodesantiago@gmail.com>
2021-10-27 13:58:39 -0500

    My first but not first java constructor...bitch!

---
+++ Classes/User.java
@@ -0,0 +1,17 @@
+package Classes;
+
+public class User {
+    public int id;
+    public String full_name;
+    public String personal_email;
+
+    //Constructor
+    public User(int id, String full_name, String personal_email) {
+        super();
+        this.id = id;
+        this.full_name = full_name;
+        this.personal_email = personal_email;
+    }
+
+
+}
